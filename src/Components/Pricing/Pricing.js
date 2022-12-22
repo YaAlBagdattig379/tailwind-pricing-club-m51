818fe938e22ef5b76b6a52a5a4015f2c404ce905
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2022-12-22 18:24:09 +0530

    about benefits of card v-6

--- src/Components/Pricing/Pricing.js
+++ src/Components/Pricing/Pricing.js
@@ -3,16 +3,33 @@ import PricingOptions from '../PringOptions/PricingOptions';
 
 const Pricing = () => {
     const pricingOptions = [
-        {id:1,name:'free',price:0},
-        {id:2,name:'regular',price:232},
-        {id:3,name:'premium',price:543}
+        {id:1,name:'free',price:0,benefits :[
+         'liftetime free',
+         'unlimited deals',
+         'localized deals',
+         'fantastic deals',
+         'crazy deals'
+        ]},
+        {id:2,name:'regular',price:99.3,benefits:[
+            'everything on regular ',
+            'unlimited deals',
+            'localized deals',
+            'fantastic deals',
+            'crazy deals' 
+        ]},
+        {id:3,name:'premium',price:32.39,benefits:[
+            'everything on premium ',
+            'unlimited deals',
+            'localized deals',
+            'fantastic deals',
+            'crazy deals' 
+        ]}
     ]
     return (
         <div className='bg-indigo-300 p-4 mt-8'>
             <h1 className='text-6xl text-white mb-5'>Best Deals of the Town</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad quas illum rerum aliquid eius a perferendis laboriosam eaque unde culpa odio velit beatae, accusantium neque, vel aperiam tempore veritatis! Voluptate!
-            Vero, molestias unde illum ut esse optio ratione. Accusantium, repellat exercitationem voluptatum odio voluptatem facere necessitatibus aperiam autem reprehenderit eveniet perspiciatis error officia fuga dolorem ex sunt vitae excepturi quae.
-            Maxime, voluptate sunt! Officiis eos eligendi similique commodi soluta id quis quae repellat, voluptatum veritatis repudiandae mollitia omnis nam illum esse explicabo eum vel provident ducimus sit placeat consequatur! Natus?</p>
+            Vero, molestias unde illum ut esse optio ratione. Accusantium, repellat exercitationem voluptatum odio voluptatem facere necessitatibus aperiam autem reprehenderit eveniet perspiciatis error officia fuga dolorem ex sunt vitae excepturi quae ?</p>
           <div className='grid md:grid-cols-3 gap-3 mt-9'>
             {
                 pricingOptions.map(options => <PricingOptions
--- src/Components/PringOptions/PricingOptions.js
+++ src/Components/PringOptions/PricingOptions.js
@@ -1,12 +1,28 @@
+import { ArrowRightIcon} from '@heroicons/react/24/solid';
 import React from 'react';
+import Benefit from '../Benefit/Benefit';
 
 const PricingOptions = (props) => {
-    const {name,price} = props.options;
+    const {name,price,benefits} = props.options;
     return (
         <div className='bg-white p-4 rounded-lg'>
             <h1 className='bg-indigo-300  py-2 rounded-lg text-xl font-bold'>{name}</h1>
-            <span className='text-5xl font-bold'>{price}</span>
-            <span className='font-bold text-xl text-gray-500'>/mon</span>
+            <p>
+                <span className='text-5xl font-bold'>{price}</span>
+                <span className='font-bold text-xl text-gray-500'>/mon</span>
+            </p>
+            <div>
+                <h3 className='text-2xl font-bold text-left'>Benefits</h3>
+                {
+                    benefits.map(benefit => <Benefit 
+                        benefit = {benefit}
+                    ></Benefit>)
+                }
+            </div>
+            <button className='bg-green-500 rounded mt-5 text-white flex justify-center gap-2 w-full py-2 hover:text-green-700 font-bold'>
+                Buy Now <ArrowRightIcon className='w-6 h-6 '></ArrowRightIcon>
+            </button>
+
         </div>
     );
 };
